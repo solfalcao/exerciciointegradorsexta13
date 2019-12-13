f4709dee95ed657b646b3d74253792b598d0322b
unknown <Digital@DH-A7-11.dhbrasil.digitalhouse.com>
2019-12-13 20:05:57 -0300

    Integrador

---
+++ src/model/livro.java
@@ -0,0 +1,13 @@
+package model;
+
+public class livro {
+
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private int anoDeLancamento;
+    private long isbn;
+    private int qntEstoque;
+    private double preco;
+
+}
